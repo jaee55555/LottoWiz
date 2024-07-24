7d06b1c64da6092bd7071641eb4375dbdb6752a8
jaee55555 <jaeeO55555@gmail.com>
2024-07-24 13:36:22 +0900

    Modify table structure

--- DataIngestion/load_2024_data.py
+++ DataIngestion/load_2024_data.py
@@ -13,7 +13,7 @@ import pymysql
 
 
 # push data to DB
-def put_data(start, end, basic_url, cur):
+def put_data(start, end, basic_url, curr):
     # take data from lotto api
     for cnt in range(start, end + 1):
         url = basic_url + str(cnt)
@@ -30,24 +30,35 @@ def put_data(start, end, basic_url, cur):
             drwt_no4 = int(data["drwtNo4"]) #당첨 번호 4
             drwt_no5 = int(data["drwtNo5"]) #당첨 번호 5
             drwt_no6 = int(data["drwtNo6"]) #당첨 번호 6
-            bnus_no = int(data["bnusNo"])   #보너스 번호
+            bnun_no = int(data["bnusNo"]) #보너스 번호
+            str_num = str(drwt_no1)+str(drwt_no2)+str(drwt_no3)+str(drwt_no4)+str(drwt_no5)+str(drwt_no6)
             str_insert = f"INSERT INTO lotto_results_2024 " \
-                         f"VALUES({drwno},'{drwno_date}',{drwt_no1},{drwt_no2},{drwt_no3}," \
-                         f"{drwt_no4}, {drwt_no5}, {drwt_no6}, {bnus_no})"
-            cur.execute(str_insert)
+                         f"VALUES({drwno},'{drwno_date}', {str_num}, " \
+                         f"{drwt_no1},{drwt_no2},{drwt_no3}," \
+                         f"{drwt_no4}, {drwt_no5}, {drwt_no6}, {bnun_no})"
+            curr.execute(str_insert)
 
 
-# print("Plz Set Start Drawing Number", end=" ")
-# N = int(sys.stdin.readline())
-# print("Plz Set Last Drawing Number", end=" ")
-# M = int(sys.stdin.readline())
+#N = start
+#M = end
 N, M = 1101, 1129
 
 basic_lotto_api_url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo="
 
-conn = pymysql.connect(host='127.0.0.1', user='ureca', password='ureca', db='lotto_data_db', charset='utf8')
-cur = conn.cursor()
-put_data(N, M, basic_lotto_api_url, cur)
 
+#접속할 db 정보 받아오기
+#DB 정보 비공개
+with open('../info.json') as f:
+    db_info = json.load(f)
+db_host = db_info["db_host"]
+db_id = db_info["db_id"]
+db_pw = db_info["db_pw"]
+db_name = db_info["db_name"]
+
+conn = pymysql.connect(host=db_host, user=db_id, password=db_pw, db=db_name, charset='utf8')
+cur = conn.cursor()
+print("Start Store Data")
+put_data(N, M, basic_lotto_api_url, cur)
+print("Finish Store Data")
 conn.commit()
 conn.close()
---
+++ DataIngestion/set_2024_db.sql
@@ -0,0 +1,27 @@
+# DB 생성 코드 파일
+-- DROP DATABASE IF EXISTS lotto_data_db;
+-- CREATE database lotto_data_db;
+
+USE lotto_data_db;
+
+# table 생성
+DROP TABLE IF EXISTS `lotto_results_2024`; 
+CREATE TABLE `lotto_results_2024` (
+`drwNo` int primary key,
+`drwNoDate` date not null,
+`numLine` varchar(13) not null,
+`drwtNo1` int not null,
+`drwtNo2` int not null,
+`drwtNo3` int not null,
+`drwtNo4` int not null,
+`drwtNo5` int not null,
+`drwtNo6` int not null,
+`bnusNo` int not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+select drwNo as "회차", drwNoDate as "추첨날짜", drwtNo1 as "1번",drwtNo2 as "2번",drwtNo3 as "3번",
+	   drwtNo4 as "4번",drwtNo5 as "5번",drwtNo6 as "6번", bnusNo as "보너스 번호" 
+from lotto_results_2024;
+DELETE FROM `lotto_results_2024`;
+
+-- describe lotto_results_2024;
